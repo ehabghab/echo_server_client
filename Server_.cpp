45a7018298a2ecb35beb52ac561def69f3aecae0
Ehab Ghabashneh <eghabash@purdue.edu>
2022-11-13 16:46:15 -0500

    [headers] clean Server headers

--- Server_.cpp
+++ Server_.cpp
@@ -1,40 +1,39 @@
 
-#include <linux/sockios.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#include <ctime>
-#include <fstream>
 #include <iostream>
 #include <thread>
 
 #define PORT 7777
 #define RESPONSE_MAX_LENGTH 1000
 
-uint8_t initializeSocket() {
+uint8_t initializeSocket()
+{
   uint8_t socketFileDescriptor;
   struct sockaddr_in address;
   int opt = 1;
 
   // Creating socket file descriptor
-  if ((socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+  if ((socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+  {
     perror("socket failed");
     exit(EXIT_FAILURE);
   }
 
   if (setsockopt(socketFileDescriptor, SOL_SOCKET, SO_REUSEADDR, &opt,
-                 sizeof(opt))) {
+                 sizeof(opt)))
+  {
     perror("setsockopt - Reuse-Address");
     exit(EXIT_FAILURE);
   }
 
   if (setsockopt(socketFileDescriptor, SOL_SOCKET, SO_REUSEPORT, &opt,
-                 sizeof(opt))) {
+                 sizeof(opt)))
+  {
     perror("setsockopt - Reuse-Port");
     exit(EXIT_FAILURE);
   }
@@ -44,7 +43,8 @@ uint8_t initializeSocket() {
   address.sin_port = htons(PORT);
 
   if (bind(socketFileDescriptor, (struct sockaddr *)&address, sizeof(address)) <
-      0) {
+      0)
+  {
     perror("bind failed");
     exit(EXIT_FAILURE);
   }
@@ -52,7 +52,8 @@ uint8_t initializeSocket() {
   return socketFileDescriptor;
 }
 
-uint8_t listenToSocket(uint8_t socketFileDescriptor) {
+uint8_t listenToSocket(uint8_t socketFileDescriptor)
+{
   std::cout << "[" << __TIME__ << " "
             << "Server_.cpp:" << __LINE__ << "]"
             << " Listening @ PORT:" << PORT << "\n";
@@ -60,12 +61,14 @@ uint8_t listenToSocket(uint8_t socketFileDescriptor) {
   int addrlen = sizeof(address);
   uint8_t newSocket;
 
-  if (listen(socketFileDescriptor, 3) < 0) {
+  if (listen(socketFileDescriptor, 3) < 0)
+  {
     perror("listen");
     exit(EXIT_FAILURE);
   }
   if ((newSocket = accept(socketFileDescriptor, (struct sockaddr *)&address,
-                          (socklen_t *)&addrlen)) < 0) {
+                          (socklen_t *)&addrlen)) < 0)
+  {
     perror("accept");
     exit(EXIT_FAILURE);
   }
@@ -75,15 +78,18 @@ uint8_t listenToSocket(uint8_t socketFileDescriptor) {
   return newSocket;
 }
 
-void sender(uint8_t socketId) {
-  while (true) {
+void sender(uint8_t socketId)
+{
+  while (true)
+  {
     // this buffer will hold the received bytes at socket.
     char *data = (char *)malloc(sizeof(char) * RESPONSE_MAX_LENGTH);
     ssize_t bytesRead;
     // fill the buffer with zeros
     bzero(data, RESPONSE_MAX_LENGTH);
     bytesRead = read(socketId, data, RESPONSE_MAX_LENGTH);
-    if (bytesRead == 0) {
+    if (bytesRead == 0)
+    {
       continue;
     }
     std::cout << "[" << __TIME__ << " "
@@ -98,7 +104,8 @@ void sender(uint8_t socketId) {
   }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   auto socketFileDescriptor = initializeSocket();
   auto socketId = listenToSocket(socketFileDescriptor);
   std::thread sendThread(sender, socketId);
